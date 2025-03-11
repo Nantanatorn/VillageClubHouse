6449ca3a35e49d6dd4ecfc1de264221bb1207479
pidchanard <pidchanard.m@ku.th>
2025-03-12 01:34:10 +0700

    fix booknow

--- Backend/Clubapi/obj/Debug/net9.0/ClubAPI.AssemblyInfo.cs
+++ Backend/Clubapi/obj/Debug/net9.0/ClubAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03eff532162fa74c6a0829780a14255236bf31cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c897b69dfbcef4596faee4d2bf5bfac03186224")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
