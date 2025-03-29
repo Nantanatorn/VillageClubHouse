a87b4ce87e1c224d3b724b22b0d24f2271737f4f
pidchanard <pidchanard.m@ku.th>
2025-03-29 21:35:10 +0700

    pull

--- Backend/Clubapi/obj/Debug/net9.0/ClubAPI.AssemblyInfo.cs
+++ Backend/Clubapi/obj/Debug/net9.0/ClubAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6449ca3a35e49d6dd4ecfc1de264221bb1207479")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b937553cff16c3984ec9bf91921c65eadb9c9a18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
